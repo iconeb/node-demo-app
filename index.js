e90355dfe62d36f7959902262f9be3267fc3ede8
iconeb@yahoo.it <iconeb@yahoo.it>
2018-10-28 13:35:05 +0100

    Added some rendering

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const Library = require('./library');
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World DevTech!');
+  res.send(Library.render('Hello World EMEA Tech!'));
 });
 
 var server = app.listen(3000, function () {
